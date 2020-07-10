6c74cb93d5b27678fe631a0d599b1025e126c7da
Julian Rudolph <julianrudolph@Julians-MacBook-Air.local>
2020-07-10 08:47:09 -0400

    Updates

---
+++ precache-manifest.0e7921a9eb0717415261aed226ec6b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71be867baa0dfbddc5d0a4082ce8d8c",
+    "url": "/havenCalc2k/index.html"
+  },
+  {
+    "revision": "8f7380231a51402957d6",
+    "url": "/havenCalc2k/static/css/main.67875bf2.chunk.css"
+  },
+  {
+    "revision": "1099696e2a0f1c92fff2",
+    "url": "/havenCalc2k/static/js/2.5faee373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/havenCalc2k/static/js/2.5faee373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7380231a51402957d6",
+    "url": "/havenCalc2k/static/js/main.22bac7f1.chunk.js"
+  },
+  {
+    "revision": "d4c8cda18362f5f271b2",
+    "url": "/havenCalc2k/static/js/runtime-main.3c3001fa.js"
+  }
+]);
